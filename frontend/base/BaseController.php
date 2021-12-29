eb44a11538e74f2183760a51eaa7e600e3be56a4
samir <samiredch@gmail.com>
2021-12-29 11:43:17 +0400

    meta tags register

--- frontend/base/BaseController.php
+++ frontend/base/BaseController.php
@@ -26,7 +26,10 @@ class BaseController extends Controller
 
   protected function setMeta($title = null, $description = null)
   {
+    $keywords = "Vektor Safety, Vektor Uniforma, Fərdi Mühafizə Vasitələri, Fərdi Qoruyucu Vasitələr, FMV, FQV, Uniforma, uniformaların satışı, xüsusi geyim, xususi geyim satisi, kaskalar, respiratorlar, İşçi geyimləri, Uniforma, təhlükəsiz, Qaz aşkarlayıcı avadanlıq, qaz aşkarlayıcı cihaz, birdəfəlik geyim, Qaynaq işləri üçün təhlükəsizlik avadanlıqları, Qaynaq işləri üçün təhlükəsizlik cihazları, Dəniz işləri üçün qoruyucu vasitələr, Yanğınsöndürənlər üçün qoruyucu vasitələr, yanğın təhlükəsizliyi vasitələri, İş aksesuarları, Gözlərin mühafizəsi, Üzün mühafizəsi, İşçi ayaqqabılar, Başın mühafizəsi, Səsqoruyucular, Tənəffüs orqanlarının mühafizəsi, Hündürlük işlərində təminat, İlk tibbi yardım vasitələri, tibbi ləvazimatlar, maye tullantılarının idarə olunması, təhlükəsizlik nişanları, Yol təhlükəsizliyinin təminatı, İşçi kombinezonlar, işçi yarımkombinezonlar, İşçi gödəkçələr, işçi şalvarlar, İşçi qış gödəkçələri, İşçi xalatlar, işçi önlüklər, İşçi jiletlər, işçi köynəklər, İşçi papaqlar, Yağmurluq geyimlər, İşçi Alt geyimləri, Mühafizəçi geyimi, İaşə işçiləri üçün geyim, Tibb işçiləri üçün geyim, Servis işçiləri üçün geyim, Reklam məqsədli geyimlər, təhükəsizlik geyimləri, təhlükəsiz işçi geyimləri, təhlükəsiz ayaqqabılar, qoruyucu işçi geyimlər, qoruyucu geyimlər, təhlükəsizlik avadanlıqları, qoruyucu əlcəklər, dəbilqələr, ayağın mühafizəsi, qoruyucu ayaqqabılar, qoruyucu eynəklər, yds botinka";
     $this->view->registerMetaTag(['name' => 'title', 'content' => "$title"]);
     $this->view->registerMetaTag(['name' => 'description', 'content' => "$description"]);
+    $this->view->registerMetaTag(['name' => 'keywords', 'content' => $keywords]);
+    $this->view->registerMetaTag(['name' => 'author', 'content' => 'Vektor Safety']);
   }
 }
